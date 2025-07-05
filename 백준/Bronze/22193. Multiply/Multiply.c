a862f26f1240299389fb366feab44f0cd2e098be
Choi Jeong-woo <151982133+sins42k@users.noreply.github.com>
2025-07-05 15:40:04 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 1112 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, a, b;
+    scanf("%d %d", &n, &m);
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int k = a * b;
+    printf("%d", k);
+    return 0;
+}
