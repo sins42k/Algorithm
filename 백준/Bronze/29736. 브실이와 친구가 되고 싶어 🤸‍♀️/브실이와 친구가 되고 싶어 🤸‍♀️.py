0c43260610bd3d043e6b2f74404c1e0d0a3f8983
Choi Jeong-woo <151982133+sins42k@users.noreply.github.com>
2025-08-03 17:11:34 +0900

    [Bronze IV] Title: 브실이와 친구가 되고 싶어 🤸‍♀️, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+k, x = map(int, input().split())
+
+cnt = 0
+
+for i in range(k-x, k+x+1):
+    if i >= a and i <= b:
+        cnt += 1
+
+if cnt == 0 : print("IMPOSSIBLE")
+else: print(cnt)
